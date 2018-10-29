cef41ed8f703c3e83a5977213828096343d05e62
Belisarius Smith <bsmith@belisariussmith.com>
2018-10-29 18:48:40 -0500

    Cleanup and version update.

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 # About TinyMUSE
 
-TinyMUSE was created in the MUD codebase explosion of 1990 and was a version of TinyMUSH (itself based on TinyMUD). It had a fairly short lifespan, and was quickly surpassed in both code quality and features by its contemporaries. Though it was updated occasionally, it was for the most part a dead project, and the TinyMUSE server population was accordingly small compared to other MUSHes. Because the code as of the last major update (v1.9f3) was still *so outdated*, it is difficult to compile, *much less run* a TinyMUSE server.
+TinyMUSE was created in the MUD codebase explosion of 1990 and was based on version 1.5 of TinyMUSH (itself based on TinyMUD). It had a fairly short lifespan, and was quickly surpassed in both code quality and features by its contemporaries. Though it was updated occasionally, it was for the most part a dead project, and the TinyMUSE server population was accordingly small compared to other MUSHes. Because the code as of the last major update (v1.9f3) was still *so outdated*, it is difficult to compile, *much less run* a TinyMUSE server.
 
-For the sake of preservation &amp; posterity, this repository has been created to provide a compilable and working version of TinyMUSE. There is no intent to add modern _mud_ functionality nor extensibility, but simply to provide a working, stable server so that someone can see how the software originally acted. To that end, some minor cleanup (such as removing support for older database versions) and improvements have been applied, such as updating to new standards (and libraries) in the C landuage to increase efficiency, viability, and modifiability.
+For the sake of preservation &amp; posterity, this repository has been created to provide a compilable and working version of TinyMUSE. There is no intent to add modern _mud_ functionality nor extensibility, but simply to provide a working, stripped down, stable server so that someone can see how the software originally acted. To that end, some minor cleanup (such as removing support for older database versions) and improvements have been applied, such as updating to new standards (and libraries) in the C landuage to increase efficiency, viability, and modifiability.
 
 This project is maintained by [Belisarius Smith](https://www.belisariussmith.com/ "Belisarius Smith").
 
@@ -12,19 +12,18 @@ This project is maintained by [Belisarius Smith](https://www.belisariussmith.com
 
 - Jin (original author)
 
-### Gen 2
+### Gen 2 (v1.0 - v1.8a4)
 - Nils McCarthy (@shkoo, **nils@chezmoto.ai.mit.edu**)
-- David Albert
+- Erk (@Erk, **erk@chezmoto.ai.mit.edu**)
+- Michael (**michael@chezmoto.ai.mit.edu**)
 
-- Ben Kotcher
-- Peter Young
-- Ken Moellman
-- Miia Kunnas
+### Gen 3 (v1.8a4 - v1.9f3)
+- Mark Eisenstat (@Morgoth **meisen@musenet.org**)
+- Jason Hula (@Power_Shaper)
+- Rick Harby (@Tabok **rharby@eznet.net**)
+- Robert Peperkamp (@Redone **rpeperkamp@envirolink.org**) 
 
-### Gen 3
-- Mark Eisenstat (@Morgoth) **meisen@musenet.org**
-
-### Gen 4
+### Gen 4 (v1.9a4 - v2.0)
 - Belisarius Smith (@Balr)
 
 This project is a work of love, in appreciation for all the hours of fun from running (and playing) my first MUD server... to all those who wrote, maintained, and played, I thank you.
@@ -35,7 +34,7 @@ See [ADMIN.md](ADMIN.md)
 
 ## Version
 
-This is currently version 2.0 (alpha)
+This is currently version 2.0 (beta)
 
 ## Original Changelog (pre-git)
 
@@ -53,4 +52,4 @@ Any new code or separate code in version 2.0+ falls under the MIT license.
 
 —
 
-<sup>NOTE: This project is still a work in progress, and will not be considered "complete" until version **2.1**</sup>
+<sup>NOTE: This project is still a work in progress, and will not be considered "complete" until version **2.0** is finalized from beta.</sup>
--- src/comm.pcmds.c
+++ src/comm.pcmds.c
@@ -13,9 +13,9 @@
 #include "config.h"
 #include "externs.h"
   
-char *base_date = BASE_DATE;
+char *base_date    = BASE_DATE;
 char *upgrade_date = UPGRADE_DATE;
-int day_release=DAY_RELEASE;
+int day_release    = DAY_RELEASE;
 
 extern long muse_up_time;
 extern long next_dump;
@@ -69,59 +69,67 @@ void do_version(dbref player)
 
 void do_uptime(dbref player)
 {
-  int index = 28;
-  long a, b=0, c=0, d=0, t;
-  static char pattern[] = "%d days, %d hrs, %d min and %d sec";
-  char format[50];
+    int index = 28;
+    long a, b=0, c=0, d=0, t;
+    static char pattern[] = "%d days, %d hrs, %d min and %d sec";
+    char format[50];
 
-  // get current time and total run time
-  a = now - muse_up_time;
+    // get current time and total run time
+    a = now - muse_up_time;
 
-  // calc seconds
-  t = a / 60L;
-  if ( t > 0L )  {
-    b = a % 60L;
-    a = t;
-    if ( a > 0L )
-      index = 17;
-
-    // calc minutes 
+    // calc seconds
     t = a / 60L;
-    if ( t > 0 )  {
-      c = b;
-      b = a % 60L;
-      a = t;
-      if ( a > 0 )
-	index = 9;
 
-      // calc hours and days 
-      t = a / 24L;
-      if ( t > 0 )  {
-	d = c;
-	c = b;
-	b = a % 24L;
-	a = t;
-	if ( a > 0 )
-	  index = 0;
-      }
+    if ( t > 0L )
+    {
+        b = a % 60L;
+        a = t;
+
+        if ( a > 0L )
+        {
+            index = 17;
+        }
+
+        // calc minutes 
+        t = a / 60L;
+
+        if ( t > 0 )
+        {
+            c = b;
+            b = a % 60L;
+            a = t;
+            if ( a > 0 )
+            {
+                index = 9;
+            }
+
+            // calc hours and days 
+            t = a / 24L;
+
+            if ( t > 0 )
+            {
+                d = c;
+                c = b;
+                b = a % 24L;
+                a = t;
+                if ( a > 0 )
+                {
+                    index = 0;
+                }
+            }
+        }
     }
-  }
 
-  // rig up format for operation time output 
-  sprintf(format, "%%s %s", &pattern[index]);
+    // rig up format for operation time output 
+    sprintf(format, "%%s %s", &pattern[index]);
 
-  send_message(player, "%s runtime stats:", muse_name);
-  send_message(player, "    Muse boot time..: %s",
-    mktm(muse_up_time, "D", player));
-  send_message(player, "    Current time....: %s",
-    mktm(now, "D", player));
-  send_message(player, format, "    In operation for:",
-    (int) a, (int) b, (int) c, (int) d);
-  send_message(player, "");
-  send_message(player, "    Next database @dump at: %s",
-         mktm(next_dump, "D", player));
-  send_message(player, "    Next old mail clear at: %s",
-         mktm(next_mail_clear, "D", player));
+    send_message(player, "%s runtime stats:", muse_name);
+    send_message(player, "    Muse boot time..: %s", mktm(muse_up_time, "D", player));
+    send_message(player, "    Current time....: %s", mktm(now, "D", player));
+    send_message(player, format, "    In operation for:", (int) a, (int) b, (int) c, (int) d);
+    send_message(player, "");
+    send_message(player, "    Next database @dump at: %s", mktm(next_dump, "D", player));
+    send_message(player, "    Next old mail clear at: %s", mktm(next_mail_clear, "D", player));
 }
 
 // uptime stuff 
@@ -139,14 +147,26 @@ static void check_time()
 {
     static struct timeval t,last;
 
-    gettimeofday(&t,NULL);
-    while (t.tv_sec != last.tv_sec) {
+    gettimeofday(&t, NULL);
+
+    while (t.tv_sec != last.tv_sec)
+    {
         if (last.tv_sec - t.tv_sec > 60*5 || t.tv_sec-last.tv_sec > 60*5)
+        {
             last.tv_sec = t.tv_sec;
+        }
         else
+        {
             last.tv_sec++;
+        }
+
         cpos++;
-        if (cpos>=60*5) cpos = 0;
+
+        if (cpos >= (60*5) )
+        {
+            cpos = 0;
+        }
+
         qcmdcnt[cpos] = pcmdcnt[cpos];
     }
 }
@@ -160,8 +180,10 @@ void inc_pcmdc()
 void do_cmdav(dbref player)
 {
     int len;
-    send_message(player,"Seconds  Player cmds/s   Queue cmds/s    Tot cmds/s");
-    for(len=5; len!=0;len=((len==5)?30:((len==30)?(60*5):0))) {
+    send_message(player, "Seconds  Player cmds/s   Queue cmds/s    Tot cmds/s");
+
+    for(len = 5; len != 0; len = ((len == 5) ? 30 : ((len == 30) ? (60*5) : 0)))
+    {
         int i;
         int cnt;
         double pcmds=0, qcmds=0;
@@ -169,14 +191,21 @@ void do_cmdav(dbref player)
 
         i = cpos-1;
         cnt = len;
-        while (cnt) {
-            if (i<=0) i = 60*5-1;
+        while (cnt)
+        {
+            if (i <= 0)
+            {
+                i = 60*5-1;
+            }
+
             pcmds += pcmdcnt[i];
             qcmds += qcmdcnt[i];
+
             i--; cnt--;
         }      
+
         sprintf(buf,"%-8d %-14f  %-14f  %f", len, pcmds/len, qcmds/len, (pcmds+qcmds)/len);
-        send_message(player,buf);
+        send_message(player, buf);
     }
 }
 
@@ -186,13 +215,20 @@ void do_at(dbref player, char *arg1, char *arg2)
     static int depth = 0;
   
     oldloc = db[player].location;
+
     if ((Typeof(player) != TYPE_PLAYER && Typeof(player) != TYPE_THING) || oldloc == NOTHING || depth > 10)
     {
-        send_message(player,perm_denied());
+        send_message(player, perm_denied());
         return;
     }
+
     newloc = match_controlled(player, arg1, POW_TELEPORT);
-    if (newloc == NOTHING) return;
+
+    if (newloc == NOTHING)
+    {
+        return;
+    }
+
     db[oldloc].contents = remove_first(db[oldloc].contents, player);
     PUSH(player, db[newloc].contents);
     db[player].location = newloc;
